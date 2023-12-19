5c75a926a8090a7c4ff2ad83127cc3de7fa152f3
Sangram Jagtap <sangramjagtap096@gmail.com>
2023-12-18 17:16:11 -0800

    Initial commit

---
+++ server/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const colors = require("colors");
+require("dotenv").config();
+const { graphqlHTTP } = require("express-graphql");
+const schema = require("./schema/schema");
+const connectDB = require("./config/db");
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+connectDB();
+
+// Only graphql endpoint unlike rest
+// graphiql - Tool similar to postman. Can be accessible on localhost:5000/graphql
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: process.env.NODE_ENV === "development",
+  })
+);
+
+app.listen(port, console.log(`Server running on port ${port}`));
