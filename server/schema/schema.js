c937c599cc0f36770b33544ca175b0bc5453065d
Sangram Jagtap <sangramjagtap096@gmail.com>
2023-12-18 19:39:45 -0800

    Added mongoose models

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,5 +1,8 @@
 const { projects, clients } = require("../sampleData.js");
 
+const Project = require("../models/Project.js");
+const Client = require("../models/Client.js");
+
 const {
   GraphQLObjectType,
   GraphQLID,
@@ -28,7 +31,8 @@ const ProjectType = new GraphQLObjectType({
     client: {
       type: ClientType,
       resolve(parent, args) {
-        return clients.find((client) => client.id === parent.clientId);
+        // return clients.find((client) => client.id === parent.clientId);
+        return Client.findById(parent.clientId);
       },
     },
   }),
@@ -49,27 +53,31 @@ const RootQuery = new GraphQLObjectType({
     projects: {
       type: new GraphQLList(ProjectType),
       resolve(parent, args) {
-        return projects;
+        // return projects;
+        return Project.find();
       },
     },
     project: {
       type: ProjectType,
       args: { id: { type: GraphQLID } },
       resolve(parent, args) {
-        return projects.find((project) => project.id === args.id);
+        // return projects.find((project) => project.id === args.id);
+        return Project.findById(args.id);
       },
     },
     clients: {
       type: new GraphQLList(ClientType),
       resolve(parent, args) {
-        return clients;
+        // return clients;
+        return Client.find();
       },
     },
     client: {
       type: ClientType,
       args: { id: { type: GraphQLID } },
       resolve(parent, args) {
-        return clients.find((client) => client.id === args.id);
+        // return clients.find((client) => client.id === args.id);
+        return Client.findById(args.id);
       },
     },
   }),
