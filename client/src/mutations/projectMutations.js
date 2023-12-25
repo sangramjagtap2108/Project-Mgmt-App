af1b7c756f44f6a90b86427b40fca591e521fa5c
Sangram Jagtap <sangramjagtap096@gmail.com>
2023-12-25 14:16:17 -0800

    Added view project and implemented add project mutation

---
+++ client/src/mutations/projectMutations.js
@@ -0,0 +1,32 @@
+import { gql } from "@apollo/client";
+
+// $status: ProjectStatus! -> ProjectStatus comes from backend(enum type)
+
+const ADD_PROJECT = gql`
+  mutation AddProject(
+    $name: String!
+    $description: String!
+    $status: ProjectStatus!
+    $clientId: ID!
+  ) {
+    addProject(
+      name: $name
+      description: $description
+      status: $status
+      clientId: $clientId
+    ) {
+      id
+      name
+      description
+      status
+      client {
+        id
+        name
+        email
+        phone
+      }
+    }
+  }
+`;
+
+export { ADD_PROJECT };
