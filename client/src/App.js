1823d1c043e5aa30b79a21698e089f9a20190c72
Sangram Jagtap <sangramjagtap096@gmail.com>
2023-12-24 17:14:53 -0800

    Added react router and pages setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,9 @@
-import AddClientModal from "./components/AddClientModal";
-import Clients from "./components/Clients";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
-import Projects from "./components/Projects";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
+import Project from "./pages/Project";
 
 // InMemoryCache - if new element gets added to list, no need to refresh
 
@@ -15,12 +16,16 @@ export default function App() {
   return (
     <>
       <ApolloProvider client={client}>
-        <Header />
-        <div className="container">
-          <AddClientModal />
-          <Projects />
-          <Clients />
-        </div>
+        <Router>
+          <Header />
+          <div className="container">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/projects/:id" element={<Project />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </div>
+        </Router>
       </ApolloProvider>
     </>
   );
